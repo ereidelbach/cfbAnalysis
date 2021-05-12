e54084777519dd9be21cd1d455464920b4059c06
Eric Reidelbach <ereidelbach@gmail.com>
2021-05-12 16:57:02 -0400

    continued development of rollup script for individual stats

--- src/data/aggregate_CFBStats_by_player_situational.py
+++ src/data/aggregate_CFBStats_by_player_situational.py
@@ -19,9 +19,11 @@ Created on Mon May 10 15:36:24 2021
 #==============================================================================
 # Package Import
 #==============================================================================
+import numpy as np
 import os  
 import pandas as pd
 import pathlib
+import tqdm
 
 from itertools import groupby   # used for grouping similar substrings
 
@@ -32,6 +34,26 @@ from itertools import groupby   # used for grouping similar substrings
 #==============================================================================
 # Function Definitions
 #==============================================================================
+def directoryCheck(team_name):
+    '''
+    Purpose: Run a check of the /data/raw/CFBStats/ folder to see if a folder
+        exists for the specified team and category. If it doesn't, create it.
+        
+    Input:
+        (1) team_name (string): Name of the school being scraped
+    
+    Output:
+        - NONE
+    '''
+    # Check for the team folder
+    pathlib.Path('data/raw/CFBStats/'+team_name).mkdir(parents=True, exist_ok=True)
+    
+    # Checking for required sub-folders
+    for category in ['games', 'players', 'records', 
+                     'rosters', 'schedules', 'situations', 'splits']:
+        pathlib.Path('data/raw/CFBStats/', team_name, category).mkdir(
+                parents=True, exist_ok=True)
+
 def roll_up_by_team(path_team):
     '''
     Purpose: Process the individual stats for each team's players. For example,
@@ -52,30 +74,340 @@ def roll_up_by_team(path_team):
     # grab a list of all files in the directory
     path_files = os.listdir(path_team)
     
+    # extract team name
+    player_team = path_files[0].split('_')[0]
+    
+    # create a list of variables that are consistent across table types
+    list_col_default_sit = ['situation', 'season', 'team', 'name', 'name_first',
+                            'name_last', 'class', 'position', 'height', 'weight',
+                            'home_town', 'home_state']
+    list_col_default_split = ['split', 'season', 'team', 'name', 'name_first',
+                              'name_last', 'class', 'position', 'height', 'weight',
+                              'home_town', 'home_state']
+    
+    # create dataframes for storing all player stats for a team
+    df_sit_team = pd.DataFrame()
+    df_split_team = pd.DataFrame()    
+    
     # group stats by player
-    for player_files in [list(playerIdx) for j, playerIdx in groupby(
-        path_files, lambda a: a.split('QB_')[1].split('_')[0])]:
+    for player_files in tqdm.tqdm([list(playerIdx) for j, playerIdx in groupby(
+        path_files, lambda a: a.split('QB_')[1].split('_')[0])]):
     
         # process PASSING situational stats
         df_sit_pass = pd.DataFrame()
         for file_sit_pass in [x for x in player_files if 'passing_situational.csv' in x]:
             df_sit_pass = df_sit_pass.append(pd.read_csv(path_team.joinpath(file_sit_pass)))
+        df_sit_pass.columns = ['pass_' + x if x not in list_col_default_sit else x for x in df_sit_pass.columns]
             
         # process RUSHING situational stats
-        df_sit_rush = pandas.DataFrame()
+        df_sit_rush = pd.DataFrame()
         for file_sit_rush in [x for x in player_files if 'rushing_situational.csv' in x]:
+            df_sit_rush = df_sit_rush.append(pd.read_csv(path_team.joinpath(file_sit_rush)))
+        df_sit_rush.columns = ['rush_' + x if x not in list_col_default_sit else x for x in df_sit_rush.columns]
             
         # process PASSING split stats
-        df_split_pass = pandas.DataFrame()
+        df_split_pass = pd.DataFrame()
         for file_split_pass in [x for x in player_files if 'passing_split.csv' in x]:
+            df_split_pass = df_split_pass.append(pd.read_csv(path_team.joinpath(file_split_pass)))
+        df_split_pass.columns = ['pass_' + x if x not in list_col_default_split else x for x in df_split_pass.columns]
             
         # process RUSHING split stats
-        df_split_rush = pandas.DataFrame()
+        df_split_rush = pd.DataFrame()
         for file_split_rush in [x for x in player_files if 'rushing_split.csv' in x]:
+            df_split_rush = df_split_rush.append(pd.read_csv(path_team.joinpath(file_split_rush)))
+        df_split_rush.columns = ['rush_' + x if x not in list_col_default_split else x for x in df_split_rush.columns]
             
-        # merge all stats together
+        # merge SITUATIONAL stats together
+        #--- handle empty dataframes by creating NaNs for all variables
+        if len(df_sit_pass) == 0:
+            df_sit_pass = create_empty_stat_dataframe('sit_pass')
+            df_sit = pd.merge(df_sit_rush, df_sit_pass, how = 'left', on = 'situation')
+        elif len(df_sit_rush) == 0:
+            df_sit_rush = create_empty_stat_dataframe('sit_rush')
+            df_sit = pd.merge(df_sit_pass, df_sit_rush, how = 'left', on = 'situation')
+        else:
+            df_sit = pd.merge(df_sit_pass, df_sit_rush, how = 'inner', on = list_col_default_sit) 
+            df_sit = df_sit[['season', 'team', 'position', 'name_last', 'name_first', 
+                             'name', 'class', 'situation', 'pass_g', 'pass_att', 
+                             'pass_comp', 'pass_pct.', 'pass_yards', 'pass_td', 
+                             'pass_int', 'pass_rating', 'pass_long', 'pass_1st', 
+                             'pass_15+', 'pass_25+', 'rush_g', 'rush_att', 
+                             'rush_yards', 'rush_avg.', 'rush_td', 'rush_long', 
+                             'rush_1st', 'rush_10+', 'rush_20+', 
+                             'height', 'weight', 'home_town', 'home_state']]
+        
+        # merge SPLIT stats together
+        #--- handle empty dataframes by creating NaNs for all variables
+        if len(df_split_pass) == 0:
+            df_split_pass = create_empty_stat_dataframe('split_pass')
+            df_split = pd.merge(df_split_rush, df_split_pass, how = 'left', on = 'split')
+        elif len(df_split_rush) == 0:
+            df_split_rush = create_empty_stat_dataframe('split_rush')
+            df_split = pd.merge(df_split_pass, df_split_rush, how = 'left', on = 'split')
+        else:
+            df_split = pd.merge(df_split_pass, df_split_rush, how = 'inner', on = list_col_default_split) 
+            df_split = df_split[['season', 'team', 'position', 'name_last', 'name_first', 
+                             'name', 'class', 'split', 'pass_g', 'pass_att', 
+                             'pass_comp', 'pass_pct.', 'pass_yards', 'pass_td', 
+                             'pass_int', 'pass_att/g', 'pass_yards/g', 
+                             'pass_yards/att', 'pass_rating', 
+                             'rush_g', 'rush_att', 'rush_yards', 'rush_avg.', 
+                             'rush_td', 'rush_att/g', 'rush_yards/g', 
+                             'height', 'weight', 'home_town', 'home_state']]
+
+        # create career totals for each player
+        df_sit, df_split = create_player_career_stats(df_sit, df_split)
+        
+        # add player stats to team dataframe
+        if len(df_sit_team) == 0:
+            df_sit_team = df_sit
+        else:
+            df_sit_team = df_sit_team.append(df_sit)
+            
+        if len(df_split_team) == 0:
+            df_split_team = df_split
+        else:
+            df_split_team = df_split_team.append(df_split)    
+            
+    # save SITUATIONAL team file to disk
+    fname_sit = f'{player_team}_situational.csv'
+    outdir_sit = pathlib.Path('data/processed/CFBStats/individual/situational')
+    if not os.path.exists(outdir_sit):
+        os.mkdir(outdir_sit)
+    df_sit_team.to_csv(outdir_sit.joinpath(fname_sit), index = False)
+        
+    # save SPLIT team file to disk
+    fname_split = f'{player_team}_split.csv'
+    outdir_split = pathlib.Path('data/processed/CFBStats/individual/split')
+    if not os.path.exists(outdir_split):
+        os.mkdir(outdir_split)
+    df_split_team.to_csv(outdir_split.joinpath(fname_split), index = False)    
+    
+    print(f'Done with: {player_team}')
+    
+    return
+
+def create_empty_stat_dataframe(df_type):
+    '''
+    Purpose: Create a placeholder "situational" or "split" stats dataframe 
+        that can be used as a stand-in if a player is lacking stats for a 
+        specific year (i.e. no rushing stats for freshman year, but QB has
+        passing stats)
+        
+    Inputs   
+    ------
+        df_type : string
+            specifies which type of dataframe to make:
+                - "sit_pass"
+                - "sit_rush"
+                - "split_pass"
+                - "split_rush"
+            
+    Outputs
+    -------
+        df_empty: pandas DataFrame
+            stats dataframe with all values initialized to NaN for all categories
+    '''
+    list_situations = ['1st Half', '2nd Half/OT', '1st Quarter', '2nd Quarter',
+                       '3rd Quarter', '4th Quarter', 'Overtime', '1st Down',
+                       '2nd Down', '3rd Down', '3rd Down, 1-3 To Go', 
+                       '3rd Down, 4-6 To Go', '3rd Down, 7-9 To Go', 
+                       '3rd Down, 10+ To Go', '4th Down', 'Own 1 To 20 Yd Ln',
+                       'Own 21 To 39 Yd Ln', 'Own 40 To Opp 40 Yd Ln', 
+                       'Opp 39 To 21 Yd Ln', 'Opp 20 To 1 Yd Ln (RZ)',
+                       'Winning By 15+ Pts', 'Winning By 8-14 Pts',
+                       'Winning By 1-7 Pts', 'Tied', 'Losing By 1-7 Pts',
+                       'Losing By 8-14 Pts', 'Losing By 15+ Pts']
+    list_sit_pass = ['pass_g', 'pass_att', 'pass_comp', 'pass_pct.', 'pass_yards', 
+                     'pass_td', 'pass_int', 'pass_rating', 'pass_long', 'pass_1st', 
+                     'pass_15+', 'pass_25+']
+    list_sit_rush = ['rush_g', 'rush_att', 'rush_yards', 'rush_avg.', 'rush_td',
+                     'rush_long', 'rush_1st', 'rush_10+', 'rush_20+']
+    
+    list_split = ['All Games', 'at Home', 'on Road/Neutral Site', 'in Wins',
+                  'in Losses', 'vs. Conference', 'vs. Non-Conference',
+                  'vs. Ranked (AP)', 'vs. Unranked (AP)', 'vs. FBS (I-A)',
+                  'vs. FCS (I-AA)', 'vs. FBS Winning', 'vs. FBS Non-Winning',
+                  'vs. BCS AQ', 'vs. BCS non-AQ', 'in August/September',
+                  'in October', 'in November', 'in December/January']
+    list_split_pass = ['pass_g', 'pass_att', 'pass_comp', 'pass_pct.', 
+                       'pass_yards', 'pass_td', 'pass_int', 'pass_att/g', 
+                       'pass_yards/g', 'pass_yards/att', 'pass_rating']
+    list_split_rush = ['rush_g', 'rush_att', 'rush_yards', 'rush_avg.', 
+                       'rush_td', 'rush_att/g', 'rush_yards/g']
+    
+    df_empty = pd.DataFrame()
+    # Handle SITUATIONAL - PASSING
+    if df_type == "sit_pass":
+        nrow = len(list_situations)
+        ncol = len(list_sit_pass)
+        df_empty = pd.DataFrame(np.zeros([nrow, ncol])*np.nan, columns=list_sit_pass)
+        df_empty['situation'] = list_situations
+    # Handle SITUATIONAL - RUSHING
+    elif df_type == "sit_rush":
+        nrow = len(list_situations)
+        ncol = len(list_sit_rush)
+        df_empty = pd.DataFrame(np.zeros([nrow, ncol])*np.nan, columns=list_sit_rush)
+        df_empty['situation'] = list_situations        
+    # Handle SPLIT - PASSING
+    elif df_type == "split_pass":
+        nrow = len(list_split)
+        ncol = len(list_split_pass)
+        df_empty = pd.DataFrame(np.zeros([nrow, ncol])*np.nan, columns=list_split_pass)
+        df_empty['split'] = list_split
+    # Handle SPLIT - PASSING
+    elif df_type == "split_rush":
+        nrow = len(list_split)
+        ncol = len(list_split_rush)
+        df_empty = pd.DataFrame(np.zeros([nrow, ncol])*np.nan, columns=list_split_rush)
+        df_empty['split'] = list_split    
+    else:
+        print("Error detected. Wrong stats type presented to 'create_empty_stat_dataframe'")
+        return
+    
+    return df_empty
+          
+def create_player_career_stats(df_sit, df_split):
+    '''
+    Purpose: Given a player's statistics broken out by year (both situational
+        and split), calculate the player's career numbers across all categories.
+        
+    Inputs   
+    ------
+        df_sit : pandas DataFrame
+            contains the pilot's situational stats for all available years
+        df_split : pandas DataFrame
+            contains the pilot's split stats for all available 
+            
+    Outputs
+    -------
+        df_sit_career : pandas DataFrame
+            the player's situational stats updated with career totals
+        df_split_career : pandas DataFrame
+            the player's split stats updated with career totals
+    '''
+    list_cols_unchanged = ['team', 'position', 'name_last', 'name_first', 'name', 
+                           'height', 'weight', 'home_town', 'home_state']
+    
+    #----- Process SITUATIONAL Stats
+    # group all situational stats by statistical category 
+    groups_sit = df_sit.groupby(['situation'])
+    
+    df_sit_career = df_sit.copy(deep = True)
+    # iterate over every category and "sum" up one by one
+    for grp in groups_sit:
+            career_sit = grp[1].sum()   
+            for col in list_cols_unchanged:
+                career_sit[col] = grp[1].iloc[-1][col]
+                career_sit['season'] = np.nan
+                career_sit['class'] = 'Career'
+                career_sit['situation'] = grp[0]
+            # add category to player's career list
+            df_sit_career = df_sit_career.append(career_sit, ignore_index = True)
+
+    # fix career situational stats for the player
+    #--- 1. Pass Rating
+    ''' NCAA passer rating is calculated by: 
+        ((8.4 x Passing Yards) + (330 x Touchdown Passes) + 
+         (100 x Number of Completions) – (200 x Interceptions)) 
+        ÷ Passing Attempts
+        
+        Sourced from: https://captaincalculator.com/sports/football/ncaa-passer-rating-calculator/
+        '''
+    df_sit_career['pass_rating'] = (((8.4 * df_sit_career['pass_yards']) + 
+                                      (330 * df_sit_career['pass_td']) + 
+                                      (100 * df_sit_career['pass_comp']) - 
+                                      (200 * df_sit_career['pass_int'])) /
+                                      df_sit_career['pass_att'])  
+    
+    #--- 2. Pass Completion %
+    df_sit_career['pass_pct.'] = df_sit_career['pass_comp']/df_sit_career['pass_att']
+    
+    #--- 3. Rush Avg (Yds/Att.)
+    df_sit_career['rush_avg.'] = df_sit_career['rush_yards']/df_sit_career['rush_att']
+                        
+    #----- Process SPLIT Stats
+    # group all split stats by statistical category 
+    groups_split = df_split.groupby(['split'])
+    
+    df_split_career = df_split.copy(deep = True)
+    # iterate over every category and "sum" up one by one
+    for grp in groups_split:
+            career_split = grp[1].sum()   
+            for col in list_cols_unchanged:
+                career_split[col] = grp[1].iloc[-1][col]
+                career_split['season'] = np.nan
+                career_split['class'] = 'Career'
+                career_split['split'] = grp[0]
+            # add category to player's career list
+            df_split_career = df_split_career.append(career_split, ignore_index = True)
+
+    # fix career situational stats for the player
+    #--- 1. Pass Rating
+    ''' NCAA passer rating is calculated by: 
+        ((8.4 x Passing Yards) + (330 x Touchdown Passes) + 
+         (100 x Number of Completions) – (200 x Interceptions)) 
+        ÷ Passing Attempts
+        
+        Sourced from: https://captaincalculator.com/sports/football/ncaa-passer-rating-calculator/
+        '''
+    df_split_career['pass_rating'] = (((8.4 * df_split_career['pass_yards']) + 
+                                      (330 * df_split_career['pass_td']) + 
+                                      (100 * df_split_career['pass_comp']) - 
+                                      (200 * df_split_career['pass_int'])) / 
+                                      df_split_career['pass_att'])
+    
+    #--- 2. Pass Completion %
+    df_split_career['pass_pct.'] = (df_split_career['pass_comp']/
+                                    df_split_career['pass_att'])
+    
+    #--- 3. Pass Att/Game
+    df_split_career['pass_att/g'] = (df_split_career['pass_att']/
+                                    df_split_career['pass_g'])
+    
+    #--- 4. Pass Yards/Game
+    df_split_career['pass_yards/g'] = (df_split_career['pass_yards']/
+                                   df_split_career['pass_g'])
+    
+    #--- 5. Rush Avg (Yds/Att.)
+    df_split_career['rush_avg.'] = (df_split_career['rush_yards']/
+                                    df_split_career['rush_att'])
+    
+    #--- 6. Rush Att/Game
+    df_split_career['rush_att/g'] = (df_split_career['rush_att']/
+                                    df_split_career['rush_g'])
+    
+    #--- 7. Rush Yards/Game
+    df_split_career['rush_yards/g'] = (df_split_career['rush_yards']/
+                                    df_split_career['rush_g'])
+            
+    return df_sit_career, df_split_career
+
+def create_files_by_year():
+    '''
+    Purpose: Loop through all team files and make a unique file that inclues
+        (1) all player data and (2) data for every year on record
+        
+    Input:
+        - NONE
+    
+    Output:
+        - NONE
+    '''
 
 def aggregate_stats():
+    '''
+    Purpose: Kickoff the process of merging all files for individual players
+        into comprehensive files that include all files for a team (including
+        player career stats) as well as stats broken out by year and a cohesive
+        file containing data for all players from all years
+        
+    Input:
+        - NONE
+    
+    Output:
+        - NONE
+    '''
     # set folder path for CFB Stats
     path_project = pathlib.Path(os.path.abspath(os.curdir), 'data', 'raw', 'CFBStats')
     
@@ -83,13 +415,12 @@ def aggregate_stats():
     list_teams = os.listdir(path_project)
     
     # roll up stats by year for each player on a team
-    for team in list_teams:
+    for team in tqdm.tqdm(list_teams):
         # create team directory path
         path_team = path_project.joinpath(team, 'individual')
         
         # roll up stats
-        roll_up_team_by_year(path_team)
-    
+        roll_up_by_team(path_team)
     
 #==============================================================================
 # Working Code
